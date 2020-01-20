00b962954e9c8c468cf1fe69c5324937ab399027
JoeyDogs <JoeyDog@163.com>
2020-01-20 16:20:03 +0800

    Create DogDome project

---
+++ src/main/java/com/joey/dog/DogdemoApplication.java
@@ -0,0 +1,13 @@
+package com.joey.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joey/dog/DogdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joey.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
