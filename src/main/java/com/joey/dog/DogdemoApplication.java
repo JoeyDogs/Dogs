2a4b4198c7520f5a95c9d501d19f695c15b4bf7c
JoeyDogs <JoeyDog@163.com>
2020-01-20 16:31:26 +0800

    update  DogDemoApplication.java

--- src/main/java/com/joey/dog/DogdemoApplication.java
+++ src/main/java/com/joey/dog/DogdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DogdemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("here");
 		SpringApplication.run(DogdemoApplication.class, args);
 	}
 
